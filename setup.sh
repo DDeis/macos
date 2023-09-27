9d4c2c7c5597fcc6a369bad61d6fecd01931b41f
DDeis <DDeis@users.noreply.github.com>
2023-09-27 18:40:09 +0200

    update

--- setup.sh
+++ setup.sh
@@ -7,11 +7,10 @@
 # # Install Homebrew
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-.
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/damien/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install deno
-
-.
 
 deno run https://raw.githubusercontent.com/DDeis/macos/main/scripts/main.ts
 # deno run https://raw.githubusercontent.com/DDeis/macos/main/scripts/main.ts?token=GHSAT0AAAAAACG2SSBFVK35DUMN72FCNKESZIUK6TA
