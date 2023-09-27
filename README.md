f2d77c28c7d06db81a5103c1bc169c0128713703
DDeis <DDeis@users.noreply.github.com>
2023-09-27 18:32:17 +0200

    update

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```sh
 /bin/bash -c "$(curl -fsSL 'https://raw.githubusercontent.com/DDeis/macos/main/setup.sh?token=GHSAT0AAAAAACG2SSBFVK35DUMN72FCNKESZIUK6TA')"
 ```
+
+```sh
+deno run https://raw.githubusercontent.com/DDeis/macos/main/scripts/main.ts?token=GHSAT0AAAAAACG2SSBFVK35DUMN72FCNKESZIUK6TA
+```
--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Install Command Line Tools
-sudo rm -rf /Library/Developer/CommandLineTools
-sudo xcode-select --install
+# # Install Command Line Tools
+# sudo rm -rf /Library/Developer/CommandLineTools
+# sudo xcode-select --install
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# # Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+.
 
 brew install deno
+
+.
 
 deno run https://raw.githubusercontent.com/DDeis/macos/main/scripts/main.ts?token=GHSAT0AAAAAACG2SSBFVK35DUMN72FCNKESZIUK6TA
